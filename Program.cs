d1dc97589defa1dde56e8c72f1b2e67a0e423423
Apetrosia <apetrosia@gmail.com>
2026-03-23 00:56:22 +0300

    Implement task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+using GreenswampRazorPages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseMiddleware<RequestLoggingMiddleware>();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseStatusCodePagesWithRedirects("/NotFound");
+
+app.MapRazorPages();
+
+app.Run();
